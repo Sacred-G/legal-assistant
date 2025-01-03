c8b94b4fa479806d2facfb11b4ffa821d8de0ffd
Steven <sgbouldin12@gmail.com>
2025-01-02 21:32:14 -0600

    Update port to 4001

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 4006;
+const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
